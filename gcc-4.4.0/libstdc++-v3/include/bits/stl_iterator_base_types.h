f6fd661dbb0734b6fe870695faa95e7db3adbc0e
gongluck <gongluck93@gmail.com>
2022-12-10 21:52:48 +0800

    std::iterator

--- gcc-4.4.0/libstdc++-v3/include/bits/stl_iterator_base_types.h
+++ gcc-4.4.0/libstdc++-v3/include/bits/stl_iterator_base_types.h
@@ -67,102 +67,110 @@
 
 _GLIBCXX_BEGIN_NAMESPACE(std)
 
-  /**
-   *  @defgroup iterators Iterators
-   *  These are empty types, used to distinguish different iterators.  The
-   *  distinction is not made by what they contain, but simply by what they
-   *  are.  Different underlying algorithms can then be used based on the
-   *  different operations supported by different iterator types.
-  */
-  //@{ 
-  ///  Marking input iterators.
-  struct input_iterator_tag { };
-  ///  Marking output iterators.
-  struct output_iterator_tag { };
-  /// Forward iterators support a superset of input iterator operations.
-  struct forward_iterator_tag : public input_iterator_tag { };
-  /// Bidirectional iterators support a superset of forward iterator
-  /// operations.
-  struct bidirectional_iterator_tag : public forward_iterator_tag { };
-  /// Random-access iterators support a superset of bidirectional iterator
-  /// operations.
-  struct random_access_iterator_tag : public bidirectional_iterator_tag { };
+/**
+ *  @defgroup iterators Iterators
+ *  These are empty types, used to distinguish different iterators.  The
+ *  distinction is not made by what they contain, but simply by what they
+ *  are.  Different underlying algorithms can then be used based on the
+ *  different operations supported by different iterator types.
+ */
+//@{
+///  Marking input iterators.
+struct input_iterator_tag // 输入迭代器标签
+{
+};
+///  Marking output iterators.
+struct output_iterator_tag // 输出迭代器标签
+{
+};
+/// Forward iterators support a superset of input iterator operations.
+struct forward_iterator_tag /*迭进迭代器标签*/ : public input_iterator_tag
+{
+};
+/// Bidirectional iterators support a superset of forward iterator
+/// operations.
+struct bidirectional_iterator_tag /*双向迭代器标签*/ : public forward_iterator_tag
+{
+};
+/// Random-access iterators support a superset of bidirectional iterator
+/// operations.
+struct random_access_iterator_tag /*可随机访问迭代器标签*/ : public bidirectional_iterator_tag
+{
+};
 
+/**
+ *  @brief  Common %iterator class.
+ *
+ *  This class does nothing but define nested typedefs.  %Iterator classes
+ *  can inherit from this class to save some work.  The typedefs are then
+ *  used in specializations and overloading.
+ *
+ *  In particular, there are no default implementations of requirements
+ *  such as @c operator++ and the like.  (How could there be?)
+ */
+template <typename _Category, typename _Tp, typename _Distance = ptrdiff_t, typename _Pointer = _Tp *, typename _Reference = _Tp &>
+struct iterator // 迭代器结构
+{
+  /// One of the @link iterator_tags tag types@endlink.
+  typedef _Category iterator_category; // 迭代器类别标签
+  /// The type "pointed to" by the iterator.
+  typedef _Tp value_type; // 元素值类型
+  /// Distance between iterators is represented as this type.
+  typedef _Distance difference_type; // 元素距离类型
+  /// This type represents a pointer-to-value_type.
+  typedef _Pointer pointer; // 元素指针类型
+  /// This type represents a reference-to-value_type.
+  typedef _Reference reference; // 元素引用类型
+};
 
-  /**
-   *  @brief  Common %iterator class.
-   *
-   *  This class does nothing but define nested typedefs.  %Iterator classes
-   *  can inherit from this class to save some work.  The typedefs are then
-   *  used in specializations and overloading.
-   *
-   *  In particular, there are no default implementations of requirements
-   *  such as @c operator++ and the like.  (How could there be?)
-  */
-  template<typename _Category, typename _Tp, typename _Distance = ptrdiff_t,
-           typename _Pointer = _Tp*, typename _Reference = _Tp&>
-    struct iterator
-    {
-      /// One of the @link iterator_tags tag types@endlink.
-      typedef _Category  iterator_category;
-      /// The type "pointed to" by the iterator.
-      typedef _Tp        value_type;
-      /// Distance between iterators is represented as this type.
-      typedef _Distance  difference_type;
-      /// This type represents a pointer-to-value_type.
-      typedef _Pointer   pointer;
-      /// This type represents a reference-to-value_type.
-      typedef _Reference reference;
-    };
+/**
+ *  This class does nothing but define nested typedefs.  The general
+ *  version simply "forwards" the nested typedefs from the Iterator
+ *  argument.  Specialized versions for pointers and pointers-to-const
+ *  provide tighter, more correct semantics.
+ */
+template <typename _Iterator>
+struct iterator_traits // 迭代器特性萃取
+{
+  typedef typename _Iterator::iterator_category iterator_category;
+  typedef typename _Iterator::value_type value_type;
+  typedef typename _Iterator::difference_type difference_type;
+  typedef typename _Iterator::pointer pointer;
+  typedef typename _Iterator::reference reference;
+};
 
-  /**
-   *  This class does nothing but define nested typedefs.  The general
-   *  version simply "forwards" the nested typedefs from the Iterator
-   *  argument.  Specialized versions for pointers and pointers-to-const
-   *  provide tighter, more correct semantics.
-  */
-  template<typename _Iterator>
-    struct iterator_traits
-    {
-      typedef typename _Iterator::iterator_category iterator_category;
-      typedef typename _Iterator::value_type        value_type;
-      typedef typename _Iterator::difference_type   difference_type;
-      typedef typename _Iterator::pointer           pointer;
-      typedef typename _Iterator::reference         reference;
-    };
+template <typename _Tp>
+struct iterator_traits<_Tp *> // 迭代器特性萃取 指针特化
+{
+  typedef random_access_iterator_tag iterator_category;
+  typedef _Tp value_type;
+  typedef ptrdiff_t difference_type;
+  typedef _Tp *pointer;
+  typedef _Tp &reference;
+};
 
-  template<typename _Tp>
-    struct iterator_traits<_Tp*>
-    {
-      typedef random_access_iterator_tag iterator_category;
-      typedef _Tp                         value_type;
-      typedef ptrdiff_t                   difference_type;
-      typedef _Tp*                        pointer;
-      typedef _Tp&                        reference;
-    };
+template <typename _Tp>
+struct iterator_traits<const _Tp *> // 迭代器特性萃取 常量指针特化
+{
+  typedef random_access_iterator_tag iterator_category;
+  typedef _Tp value_type;
+  typedef ptrdiff_t difference_type;
+  typedef const _Tp *pointer;
+  typedef const _Tp &reference;
+};
 
-  template<typename _Tp>
-    struct iterator_traits<const _Tp*>
-    {
-      typedef random_access_iterator_tag iterator_category;
-      typedef _Tp                         value_type;
-      typedef ptrdiff_t                   difference_type;
-      typedef const _Tp*                  pointer;
-      typedef const _Tp&                  reference;
-    };
+/**
+ *  This function is not a part of the C++ standard but is syntactic
+ *  sugar for internal library use only.
+ */
+template <typename _Iter>
+inline typename iterator_traits<_Iter>::iterator_category __iterator_category(const _Iter &)
+{
+  return typename iterator_traits<_Iter>::iterator_category();
+}
 
-  /**
-   *  This function is not a part of the C++ standard but is syntactic
-   *  sugar for internal library use only.
-  */
-  template<typename _Iter>
-    inline typename iterator_traits<_Iter>::iterator_category
-    __iterator_category(const _Iter&)
-    { return typename iterator_traits<_Iter>::iterator_category(); }
-
-  //@}
+//@}
 
 _GLIBCXX_END_NAMESPACE
 
 #endif /* _STL_ITERATOR_BASE_TYPES_H */
-
